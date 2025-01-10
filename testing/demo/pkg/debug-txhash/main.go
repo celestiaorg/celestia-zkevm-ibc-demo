d6bc84da91b567e4af57d7c4a42a3238e5612d7a
Rootul Patel <rootulp@gmail.com>
2025-01-10 17:06:10 -0500

    figured out revert reason!

--- testing/demo/pkg/debug-txhash/main.go
+++ testing/demo/pkg/debug-txhash/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 
@@ -8,22 +9,22 @@ import (
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
-func getRevertReason(txHash ethcommon.Hash, rpcURL string) string {
+func getRevertReason(txHash ethcommon.Hash, rpcURL string) []byte {
 	client, err := rpc.Dial(rpcURL)
 	if err != nil {
 		log.Fatalf("Failed to connect to RPC: %v", err)
 	}
-	var result string
-	err = client.Call(&result, "debug_traceTransaction", txHash.Hex(), map[string]interface{}{})
+	var raw json.RawMessage
+	err = client.Call(&raw, "debug_traceTransaction", txHash.Hex(), map[string]interface{}{})
 	if err != nil {
 		log.Fatalf("Failed to trace transaction: %v", err)
 	}
-	return result
+	return raw
 }
 
 func main() {
-	txHash := ethcommon.HexToHash("0x4fa612350e69087839ac4dcb60247d93bc2f739de618bc81829208c21a18a19d")
+	txHash := ethcommon.HexToHash("0xdbbe9a1c3569ca3c0274bb0b6d01d85af5d708437d44b6de7658eeec08cb6dbe")
 	rpcURL := "http://localhost:8545/"
 	result := getRevertReason(txHash, rpcURL)
-	fmt.Printf("result %v\n", result)
+	fmt.Printf("result %v\n", string(result))
 }
