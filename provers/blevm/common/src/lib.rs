82be91345982210e7a41c730832019bf0fa95c3d
Rootul P <rootulp@gmail.com>
2025-01-27 10:36:40 -0700

    chore: move blevm into this repo (#91)
    Closes https://github.com/celestiaorg/celestia-zkevm-ibc-demo/issues/74
    
    FLUPs: 
    - [ ] https://github.com/celestiaorg/celestia-zkevm-ibc-demo/issues/92
    - [ ] https://github.com/celestiaorg/celestia-zkevm-ibc-demo/issues/93
    - [ ] https://github.com/celestiaorg/celestia-zkevm-ibc-demo/issues/90

---
+++ provers/blevm/common/src/lib.rs
@@ -0,0 +1,22 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct BlevmOutput {
+    pub blob_commitment: [u8; 32],
+    pub header_hash: [u8; 32],
+    pub prev_header_hash: [u8; 32],
+    pub height: u64,
+    pub gas_used: u64,
+    pub beneficiary: [u8; 20],
+    pub state_root: [u8; 32],
+    pub celestia_header_hash: [u8; 32],
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct BlevmAggOutput {
+    pub newest_header_hash: [u8; 32],
+    pub oldest_header_hash: [u8; 32],
+    pub celestia_header_hashes: Vec<[u8; 32]>,
+    pub newest_state_root: [u8; 32],
+    pub newest_height: u64,
+}
