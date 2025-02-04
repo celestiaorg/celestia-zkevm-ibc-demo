7e8f7b6dd4ae053caa58461aed6d73b21e01a3e6
Rootul Patel <rootulp@gmail.com>
2025-02-03 23:40:06 -0500

    fix: more informative error message

--- provers/blevm/script/src/bin/main.rs
+++ provers/blevm/script/src/bin/main.rs
@@ -78,7 +78,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
         // Hardcode the height of the block containing the blob
         // https://celenium.io/blob?commitment=eUbPUo7ddF77JSASRuZH1arKP7Ur8PYGtpW0qwvTP0w%3D&hash=AAAAAAAAAAAAAAAAAAAAAAAAAA8PDw8PDw8PDw8%3D&height=2988873
         block_height: 2988873,
-        l2_block_data: fs::read("input/1/18884864.bin")?,
+        l2_block_data: fs::read("input/1/18884864.bin").expect(
+            "Failed to load L2 block data. Ensure you're in a directory with input/1/18884864.bin",
+        ),
     };
 
     if args.execute {
