08c98b9f3d24b37ea835da1ddfe3e89ac21afe2e
Rootul Patel <rootulp@gmail.com>
2025-02-03 20:01:43 -0500

    chore: disable mock blevm elf

--- provers/blevm/script/build.rs
+++ provers/blevm/script/build.rs
@@ -2,5 +2,5 @@ use sp1_helper::build_program_with_args;
 
 fn main() {
     build_program_with_args("../blevm", Default::default());
-    build_program_with_args("../blevm-mock", Default::default());
+    // build_program_with_args("../blevm-mock", Default::default());
 }
