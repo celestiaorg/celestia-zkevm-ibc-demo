44c789321586b9cc6c2320432b27d4ab3f072ea0
Rootul P <rootulp@gmail.com>
2025-02-04 22:20:12 -0700

    refactor: consolidate celestia-prover (#135)
    Closes https://github.com/celestiaorg/celestia-zkevm-ibc-demo/issues/109
    
    ## Testing
    
    ```
    $ cargo run -p celestia-prover
    ```
    
    starts the server and I can query verifier keys:
    
    ```
    $     grpcurl -plaintext localhost:50051 celestia.prover.v1.Prover/Info
    {
      "stateTransitionVerifierKey": "0x007279d4076a55500ad2d12fbcb6e336958713327a6020417094a5210eb4d455",
      "stateMembershipVerifierKey": "0x003e36b7393697dd4a69cf0c660974825932095295a4fbbe0bab7982dfbffda7"
    }
    ```

--- provers/celestia-prover/prover/build.rs
+++ provers/celestia-prover/prover/build.rs
@@ -2,6 +2,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
         .file_descriptor_set_path("proto_descriptor.bin")
-        .compile_protos(&["../../proto/prover/v1/prover.proto"], &["../../proto"])?;
+        .compile_protos(
+            &["../../../proto/prover/v1/prover.proto"],
+            &["../../../proto"],
+        )?;
     Ok(())
 }
