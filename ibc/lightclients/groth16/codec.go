e2828d6675239c3b1f4f5f534693b522ff894b4c
Rootul Patel <rootulp@gmail.com>
2025-02-15 06:02:42 -0500

    fix: comment

--- ibc/lightclients/groth16/codec.go
+++ ibc/lightclients/groth16/codec.go
@@ -2,11 +2,10 @@ package groth16
 
 import (
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
-
 	"github.com/cosmos/ibc-go/v9/modules/core/exported"
 )
 
-// RegisterInterfaces registers the tendermint concrete client-related
+// RegisterInterfaces registers the concrete groth16 client-related
 // implementations and interfaces.
 func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 	registry.RegisterImplementations(
