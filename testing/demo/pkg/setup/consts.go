033e04eeba0361264e5c531b81f618448f228196
Rootul Patel <rootulp@gmail.com>
2025-03-12 15:27:40 -0400

    refactor: remove unused const

--- testing/demo/pkg/setup/consts.go
+++ testing/demo/pkg/setup/consts.go
@@ -15,8 +15,6 @@ const (
 	simappRPC = "http://localhost:5123"
 	// ethereumRPC is the URI for the ethereum node.
 	ethereumRPC = "http://localhost:8545"
-	// celestiaProverRPC is the URI for the celestia prover.
-	celestiaProverRPC = "localhost:50052"
 )
 
 var (
