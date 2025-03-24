157e65ebc7474c0e9a7d92ce7b9df60375a3ef65
Rootul Patel <rootulp@gmail.com>
2025-03-24 14:27:47 -0400

    fix: create groth16 client with evm prover STF verifier key

--- testing/demo/pkg/setup/consts.go
+++ testing/demo/pkg/setup/consts.go
@@ -2,6 +2,11 @@ package main
 
 import "math/big"
 
+var (
+	ethChainId   = big.NewInt(80087)
+	merklePrefix = [][]byte{[]byte("ibc"), []byte("")}
+)
+
 const (
 	// groth16ClientID is for the Ethereum light client on the SimApp.
 	groth16ClientID = "08-groth16-0"
@@ -11,13 +16,14 @@ const (
 	ethPrivateKey = "0x82bfcfadbf1712f6550d8d2c00a39f05b33ec78939d0167be2a737d691f33a6a"
 	// relayer is the address registered on simapp
 	relayer = "cosmos1ltvzpwf3eg8e9s7wzleqdmw02lesrdex9jgt0q"
-	// simappRPC is the URI for the simapp node.
-	simappRPC = "http://localhost:5123"
-	// ethereumRPC is the URI for the ethereum node.
-	ethereumRPC = "http://localhost:8545"
 )
 
-var (
-	ethChainId   = big.NewInt(80087)
-	merklePrefix = [][]byte{[]byte("ibc"), []byte("")}
+// RPC endpoints
+const (
+	// ethereumRPC is the Reth RPC endpoint.
+	ethereumRPC = "http://localhost:8545/"
+	// evmProverRPC is the RPC endpoint for the EVM prover.
+	evmProverRPC = "localhost:50052"
+	// simappRPC is the URI for the simapp node.
+	simappRPC = "http://localhost:5123"
 )
--- testing/demo/pkg/transfer/consts.go
+++ testing/demo/pkg/transfer/consts.go
@@ -1,25 +1,41 @@
 package main
 
+// Tokens
 const (
 	// denom is the denomination of the token on SimApp.
 	denom = "stake"
+)
+
+// Addresses
+const (
 	// sender is an address on SimApp that will send funds via the MsgTransfer.
 	sender = "cosmos1ltvzpwf3eg8e9s7wzleqdmw02lesrdex9jgt0q"
 	// receiver is an address on the EVM chain that will receive funds via the MsgTransfer.
 	receiver = "0x7f39c581f595b53c5cb19b5a6e5b8f3a0b1f2f6e"
-	// ethPrivateKey is the private key for ethereumAddress.
-	ethPrivateKey = "0x82bfcfadbf1712f6550d8d2c00a39f05b33ec78939d0167be2a737d691f33a6a"
+)
 
+// Client IDs
+const (
 	// tendermintClientID is for the SP1 Tendermint light client on the EVM roll-up.
 	tendermintClientID = "07-tendermint-0"
 	// groth16ClientID is for the Ethereum light client on the SimApp.
 	groth16ClientID = "08-groth16-0"
+)
 
+// RPC endpoints
+const (
 	// ethereumRPC is the Reth RPC endpoint.
 	ethereumRPC = "http://localhost:8545/"
 	// celestiaProverRPC is the RPC endpoint for the Celestia prover.
 	celestiaProverRPC = "localhost:50051"
+	// evmProverRPC is the RPC endpoint for the EVM prover.
+	evmProverRPC = "localhost:50052"
 )
 
-// ethereumAddress is an address on the EVM chain.
-// _ethereumAddress = "0xaF9053bB6c4346381C77C2FeD279B17ABAfCDf4d"
+// Keys
+const (
+	// ethPrivateKey is the private key for ethereumAddress.
+	ethPrivateKey = "0x82bfcfadbf1712f6550d8d2c00a39f05b33ec78939d0167be2a737d691f33a6a"
+	// ethereumAddress is an address on the EVM chain.
+	// _ethereumAddress = "0xaF9053bB6c4346381C77C2FeD279B17ABAfCDf4d"
+)
