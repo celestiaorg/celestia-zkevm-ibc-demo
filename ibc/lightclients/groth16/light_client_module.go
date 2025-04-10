afa9c550685494f64d48a5ec98081cd08c5b33d7
Rootul P <rootulp@gmail.com>
2025-04-10 05:24:42 -0400

    feat: update groth16 client (#239)
    Closes https://github.com/celestiaorg/celestia-zkevm-ibc-demo/issues/221
    
    Note this uses an empty proof. A follow up is to make this step use mock
    proofs generated by evm prover.
    
    ## Testing
    
    ```shell
    make transfer
    make transfer-back
    
    $ make transfer-back
    --> Transferring tokens back from the EVM roll-up to simapp
    Allowed 1 tokens to be spent by the ICS20Transfer contract
    Submit transfer back msg successfully tx hash: 0xa0395dc7a76600de94e5f9be54ff42d619a368273ee008865f5b38bc4a5620e6
    Updating Groth16 light client on EVM roll-up...
    Groth16 light client current height 608 and state root 079389CC285F5E1835F21C8B206738FA418E3F6EED4BCC3D064A514761AB9B3F
    Waiting for transaction F76690191EAF8DC2B75EC8B842B86398BD6B6DAC0DCAAC18E9409E33574F8604 to land in a block...
    Transaction landed in block 140 with code 0
    Updated Groth16 light client on simapp. New height: 676 state root F070C5D54170D3E91BBFBC32C4A85C972CAEB96C1256139B2AFDF01DE88A0F14
    ```

--- ibc/lightclients/groth16/light_client_module.go
+++ ibc/lightclients/groth16/light_client_module.go
@@ -73,7 +73,7 @@ func (l LightClientModule) CheckForMisbehaviour(ctx sdktypes.Context, clientID s
 func (l LightClientModule) UpdateStateOnMisbehaviour(ctx sdktypes.Context, clientID string, clientMsg exported.ClientMessage) {
 }
 
-// UpdateState obtains the client state associated with the client identifier and calls into the clientState.UpdateState method.
+// UpdateState obtains the client state associated with the client identifier and calls into the clientState.UpdateConsensusState method.
 func (l LightClientModule) UpdateState(ctx sdktypes.Context, clientID string, clientMsg exported.ClientMessage) []exported.Height {
 	clientStore := l.storeProvider.ClientStore(ctx, clientID)
 	clientState, found := getClientState(clientStore, l.cdc)
@@ -81,7 +81,12 @@ func (l LightClientModule) UpdateState(ctx sdktypes.Context, clientID string, cl
 		panic(errorsmod.Wrap(clienttypes.ErrClientNotFound, clientID))
 	}
 
-	return clientState.UpdateState(ctx, l.cdc, clientStore, clientMsg)
+	height, err := clientState.UpdateState(ctx, l.cdc, clientStore, clientMsg)
+	if err != nil {
+		panic(err)
+	}
+
+	return height
 }
 
 // VerifyMembership obtains the client state associated with the client identifier and calls into the clientState.verifyMembership method.
