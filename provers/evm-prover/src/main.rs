f75c3825996a0f532ae4916112725664d497892b
Rootul Patel <rootulp@gmail.com>
2025-04-18 12:31:47 -0400

    chore(evm-prover): use sp1 mock mode

--- provers/evm-prover/src/main.rs
+++ provers/evm-prover/src/main.rs
@@ -249,7 +249,9 @@ impl Prover for ProverService {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv().ok();
-
+    if let Ok(mode) = env::var("SP1_PROVER") {
+        println!("SP1_Prover mode: {mode}");
+    };
     let addr = "[::]:50052".parse()?;
     let prover = ProverService::new().await?;
 
