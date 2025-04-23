c665091e4f26a8b77c6af67f18a80f7488d242c7
Nina Barbakadze <barbakadzeninaa@gmail.com>
2025-04-23 16:20:38 +0200

    fix: lint

--- testing/demo/pkg/ethereum/ethereumtypes.go
+++ testing/demo/pkg/ethereum/ethereumtypes.go
@@ -4,8 +4,6 @@ import (
 	"time"
 
 	"github.com/attestantio/go-eth2-client/spec/phase0"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
 type Spec struct {
