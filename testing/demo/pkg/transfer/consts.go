d484162fd4c3782739a5dfb71c9b36a7de18f0a1
Rootul P <rootulp@gmail.com>
2025-04-25 11:27:15 -0400

    feat: use evm-prover mock proofs (#257)
    Closes https://github.com/celestiaorg/celestia-zkevm-ibc-demo/issues/245

--- testing/demo/pkg/transfer/consts.go
+++ testing/demo/pkg/transfer/consts.go
@@ -45,4 +45,6 @@ const (
 	ethereumRPC = "http://localhost:8545/"
 	// celestiaProverRPC is the RPC endpoint for the Celestia prover.
 	celestiaProverRPC = "localhost:50051"
+	// evmProverRPC is the RPC endpoint for the EVM prover.
+	evmProverRPC = "localhost:50052"
 )
